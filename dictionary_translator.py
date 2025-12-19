3b5b22250da3bd62342cd15cf59cb20a6b767888
ogbozorkamsi-coder <ogbozorkamsi@gmail.com>
2025-12-19 20:38:27 +0100

    Add files via upload

---
+++ dictionary_translator.py
@@ -0,0 +1,23 @@
+igbo_dict = {# by Daniel Ogbozor
+    "book": "akwụkwọ",
+    "chair": "oche",
+    "table": "tebụl",
+    "car": "ụgbọ ala",
+    "house": "ụlọ",
+    "pen": "mpeni",
+    "phone": "ekwentị",
+    "shoe": "akpụkpọ ụkwụ",
+    "water": "mmiri",
+    "food": "nri",
+    "tree": "osisi",
+    "dog": "nkịta",
+    "cat": "pusi",
+    "bag": "akpa",
+    "door": "ụzọ",
+    "window": "ụlọ ọnụ ụzọ",
+    "bed": "ụlọikwuu",
+    "shirt": "uwe elu",
+    "money": "ego",
+    "road": "ụzọ"
+}
+
